830cce9e2a53e81bd0e0ccc144c897fdbb67bcc4
Casper Verswijvelt <casperverswijvelt@gmail.com>
2022-09-11 02:42:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+// Use JSON file for storage
+const file = join(dirname(fileURLToPath(import.meta.url)), 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+// Read data from JSON file, this will set db.data content
+await db.read()
+
+// If file.json doesn't exist, db.data will be null
+// Set default data
+// db.data = db.data || { posts: [] } // Node < v15.x
+db.data ||= { reports: [] }             // Node >= 15.x
+
+// Finally write db.data content to file
+await db.write()
+
+const app = express();
+app.use(express.json());
+
+app.post("/report", async (req, res) => {
+
+    try {
+
+
+        db.data.reports.push({
+            timestamp: Date.now(),
+            ...req.body
+        })
+        await db.write()
+
+        res.sendStatus('200')
+
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e);
+    }
+});
+
+// Start listening for requests
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
